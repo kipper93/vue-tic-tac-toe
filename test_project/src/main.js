099cd3261d68e94228a2e39bbd349cb8dd2af566
刘明艳 <yep@liumingyandeMacBook-Pro.local>
2022-08-31 23:06:43 +0800

    element 改为按需引用

--- test_project/src/main.js
+++ test_project/src/main.js
@@ -3,12 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
+import { Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.prototype.$message = Message
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
